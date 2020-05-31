fff1555e91961a3b24f19cd163b466ec0a2b389c
Zhang XH <39736627+timegold-websrc@users.noreply.github.com>
2020-05-31 11:35:54 +0800

    Init project

---
+++ src/index.js
@@ -0,0 +1,14 @@
+var cors = require('cors');
+var express = require('express');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3002, () =>
+  console.log(`Example app listening on port 3002!`),
+);
