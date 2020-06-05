62833d4ced44f83819077b3f1acbec7ee1ec48ad
Zhang XH <39736627+timegold-websrc@users.noreply.github.com>
2020-06-06 00:25:31 +0800

    updated port

--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(3002, () =>
-  console.log(`Example app listening on port 3002!`),
+app.listen(3000, () =>
+  console.log(`Example app listening on port 3000!`),
 );
